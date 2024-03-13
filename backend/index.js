df1cad97a0586ae1a328e657b77ba203cce54b33
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2024-03-14 03:13:34 +0530

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(() => {
+    console.log("MongoDB is connected");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const app = express();
+const PORT = process.env.PORT || 4000
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
