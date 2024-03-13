c250ebbacd04423e496c6e52c5d16523d5df079c
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2024-03-14 04:08:21 +0530

    create a test api route

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,14 @@ mongoose.connect(process.env.DATABASE_URL)
 });
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+// create a api -> 
+
+const userRouter = require('./routes/user.route')
+app.use('/api/user', userRouter);
