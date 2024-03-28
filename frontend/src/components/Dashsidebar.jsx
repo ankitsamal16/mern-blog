59f31d6b2c8ef0b308d94b857f783e2817d29c9b
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2024-03-28 12:15:32 +0530

    complete user image upload

--- frontend/src/components/Dashsidebar.jsx
+++ frontend/src/components/Dashsidebar.jsx
@@ -18,13 +18,13 @@ export default function Dashsidebar() {
         <Sidebar.Items>
             <Sidebar.ItemGroup>
                 <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark' as='div'>
                         Profile
                     </Sidebar.Item>
-                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
-                        Sign Out
-                    </Sidebar.Item>
                 </Link>
+                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                    Sign Out
+                </Sidebar.Item>
             </Sidebar.ItemGroup>
         </Sidebar.Items>
     </Sidebar>
